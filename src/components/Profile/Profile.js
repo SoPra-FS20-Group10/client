ed263d45450f54638e9d1ffa88ead40ee2d2a351
Wassermalone <pascal.marty98@gmail.com>
2020-04-06 20:35:52 +0200

    Implemented buttons for editing credentials
    FS20G10-53
    
    Added buttons with popups to change username & password. Not yet functional. Also changed temporary background to something more neutral, still temporary though.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,6 +7,8 @@ import LobbyList from "../Lobby/LobbyList";
 import Button from "react-bootstrap/Button";
 import Chart from "./Chart";
 import NavigationBar from "../../views/NavigationBar";
+import {CloseButton} from "react-bootstrap";
+import Modal from "react-modal";
 
 const StatsWrapper = styled.div`
     margin-top: 5%;
@@ -30,7 +32,6 @@ const GraphWrapper = styled.div`
      
 `;
 
-
 const MatchHistoryWrapper = styled.div`
     margin-top: 5%;
     margin-left: 5%;
@@ -49,8 +50,65 @@ const Container = styled(BaseContainer)`
 
 const ButtonContainer = styled.div`
     width: 33.3%;
+`;
+
+const ButtonContainer3 = styled.div`
+    margin-top: 2%;
+    margin-left: 5%;
     float:left;
 `;
+
+const CredentialsPopupWrapper = styled.div`
+    background-color: white;
+`;
+
+const Title = styled.div`
+    background-color: grey;
+
+`;
+
+const Label = styled.label`
+  color: black;
+  text-transform: uppercase;
+  font-size:16pt;
+  background-color: grey;
+   margin-left: 5%;
+`;
+
+const InputField = styled.input`
+  &::placeholder {
+    color: grey;
+  }
+  height: 35px;
+  padding-left: 15px;
+  border: none;
+  border-radius: 20px;
+  background: rgba(77, 77, 77, 0.2);
+  color: grey;
+
+  margin: 0 auto;
+
+`;
+
+const InputFieldWrapper = styled.div`
+     margin-top: 10pt;
+    margin-bottom: 10pt;
+    text-align: center;
+
+`;
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        padding: "0pt",
+        width: "55%"
+    }
+};
 
 class Profile extends React.Component {
     constructor() {
@@ -60,19 +118,27 @@ class Profile extends React.Component {
             stats: {id: 1, name: 'HOTCHILIEATER', wins: 1000, winPercentage: '59%', timePlayed: '370h'}
 
         }
+
+        this.handleOpenModalName = this.handleOpenModalName.bind(this);
+        this.handleCloseModalName = this.handleCloseModalName.bind(this);
+        this.handleOpenModalPassword = this.handleOpenModalPassword.bind(this);
+        this.handleCloseModalPassword = this.handleCloseModalPassword.bind(this);
     }
 
-    goToMainPage = () => {
-        this.props.history.push("/game/overview");
+    handleOpenModalName() {
+        this.setState({showModalName: true});
     }
 
-    goToLeaderboard = () => {
-        this.props.history.push("/game/leaderboard");
+    handleCloseModalName() {
+        this.setState({showModalName: false});
     }
 
-    goToProfile = () => {
-        // TODO: Redirect with user's ID
-        this.props.history.push(`/game/profile/${1}`);
+    handleOpenModalPassword() {
+        this.setState({showModalPassword: true});
+    }
+
+    handleCloseModalPassword() {
+        this.setState({showModalPassword: false});
     }
 
     render() {
@@ -98,8 +164,87 @@ class Profile extends React.Component {
                 </MatchHistoryWrapper>
 
 
+                {/*Buttons for editing credentials*/}
+                <ButtonContainer3>
+                    <Button variant="dark" size="lg" onClick={this.handleOpenModalName}>
+                        Change Name
+                    </Button> {' '}
+                    <Button variant="dark" size="lg" onClick={this.handleOpenModalPassword}>
+                        Change Password
+                    </Button>
+
+                </ButtonContainer3>
+
+                {/*Modal for Name-Popup*/}
+                <Modal
+                    isOpen={this.state.showModalName}
+                    contentLabel="Inline Styles Modal Example"
+                    style={customStyles}
+                >
+                    <CredentialsPopupWrapper>
+                        <CloseButton onClick={this.handleCloseModalName}/>
+                        <Title>
+                            <Label>Change Username</Label>
+                        </Title>
+                        {/*<MyInputLabel> LOBBY NAME</MyInputLabel>*/}
+                        <InputFieldWrapper>
+                            <InputField
+                                placeholder="New Username"
+                                onChange={e => {
+                                    this.handleInputChange('tempUsername', e.target.value);
+                                }}/>
+                        </InputFieldWrapper>
+                    </CredentialsPopupWrapper>
+                    <ButtonContainer>
+                        <Button variant="dark" size="sm" block onClick={this.changeName()}>
+                            Save Changes
+                        </Button>
+                    </ButtonContainer>
+                </Modal>
+
+                {/*Modal for Password-Popup*/}
+                <Modal
+                    isOpen={this.state.showModalPassword}
+                    contentLabel="Inline Styles Modal Example"
+                    style={customStyles}
+                >
+                    <CredentialsPopupWrapper>
+                        <CloseButton onClick={this.handleCloseModalPassword}/>
+                        <Title>
+                            <Label>Change Password</Label>
+                        </Title>
+                        <InputFieldWrapper>
+                            <InputField
+                                placeholder="New Password"
+                                onChange={e => {
+                                    this.handleInputChange('tempUsername', e.target.value);
+                                }}/>
+                        </InputFieldWrapper>
+                        <InputFieldWrapper>
+                            <InputField
+                                placeholder="Repeat Password"
+                                onChange={e => {
+                                    this.handleInputChange('tempUsername', e.target.value);
+                                }}/>
+                        </InputFieldWrapper>
+                    </CredentialsPopupWrapper>
+                    <ButtonContainer>
+                        <Button variant="dark" size="sm" block onClick={this.changePassword()}>
+                            Save Changes
+                        </Button>
+                    </ButtonContainer>
+                </Modal>
+
             </Container>
         );
+    }
+
+    changeName() {
+
+    }
+
+    changePassword() {
+
     }
 }
 
