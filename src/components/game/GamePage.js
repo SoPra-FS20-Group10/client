28fc3617000d07dd3bc8cbf3ac71349d9486b1d1
Patrick Looser <palooser.rng@gmail.com>
2020-04-22 14:28:01 +0200

    Moved Gameboard to GamePage

--- src/components/game/GamePage.js
+++ src/components/game/GamePage.js
@@ -22,10 +22,16 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container'
 import {DndProvider} from "react-dnd";
-import Backend from "react-dnd-html5-backend"
+import Backend, {NativeTypes} from "react-dnd-html5-backend"
 import ScoreBoard from "./ScoreBoard";
 import Modal from "react-modal";
 import {CloseButton} from "react-bootstrap";
+import Grid from "@material-ui/core/Grid";
+import Square from "./Square";
+import LetterBox from "./LetterBox";
+import ItemTypes from "./ItemTypes";
+import PIECE from "../shared/Other/Pieces";
+import TILES from "../shared/Other/Tiles";
 
 
 // const Container = styled(BaseContainer)`
@@ -75,6 +81,31 @@ const DeckWrapper = styled.div`
     align-items: center;
     justify-content: center;
 `;
+const PieceWrapper = styled.div`
+    margin: 2pt;
+    width: 27pt;
+    height: 27pt;
+    
+    background: white;
+    color: black;
+    justify-content:center;
+    align-items: center;
+    display: flex;
+`;
+
+const TileWrapper = styled.div`
+    margin: 2pt;
+    width: 27pt;
+    height: 27pt;
+   
+    background: white;
+    color: black;
+    justify-content:center;
+    align-items: center;
+    display: flex;
+`;
+
+
 const customStyles = {
     content : {
         top                   : '50%',
@@ -87,6 +118,7 @@ const customStyles = {
         width                 : "55%"
     }
 };
+
 class GamePage extends React.Component {
     constructor(props) {
         super(props);
@@ -100,9 +132,51 @@ class GamePage extends React.Component {
             showMainPage: true,
             showLeaderboard: false,
             showProfile: false,
+            gameId: localStorage.getItem("currentGame"),
+            dustbins: [
+                {accepts: [ItemTypes.TILE], lastDroppedItem: null},
+                {accepts: [ItemTypes.TILE], lastDroppedItem: null},
+                {
+                    accepts: [ItemTypes.TILE, ItemTypes.GLASS, NativeTypes.URL],
+                    lastDroppedItem: null,
+                },
+                {accepts: [ItemTypes.TILE, NativeTypes.FILE], lastDroppedItem: null},
+            ],
+            boxes: [
+                { piece: PIECE.W},
+                { piece: PIECE.E},
+                { piece: PIECE.S},
+                { piece: PIECE.Q},
+                { piece: PIECE.C},
+                { piece: PIECE.G},
+                { piece: PIECE.F},
+
+            ],
+            droppedBoxNames: [],
+            board: [
+                [{piece: null, type: TILES.TW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TW}],
+                [{piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}],
+                [{piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}],
+                [{piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}],
+                [{piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}],
+                [{piece: null, type: TILES.NT}, {piece: null, type: TILES.TL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TL}, {piece: null, type: TILES.NT}],
+                [{piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}],
+                [{piece: null, type: TILES.TW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.ST}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TW}],
+                [{piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}],
+                [{piece: null, type: TILES.NT}, {piece: null, type: TILES.TL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TL}, {piece: null, type: TILES.NT}],
+                [{piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}],
+                [{piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}],
+                [{piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}],
+                [{piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DW}, {piece: null, type: TILES.NT}],
+                [{piece: null, type: TILES.TW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TW}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.DL}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.NT}, {piece: null, type: TILES.TW}],
+            ],
+
         };
         this.handleOpenModal = this.handleOpenModal.bind(this);
         this.handleCloseModal = this.handleCloseModal.bind(this);
+        this.handleDrop=this.handleDrop.bind(this);
+        this.isDropped=this.isDropped.bind(this);
+        this.drawTile=this.drawTile.bind(this);
 
     }
 
@@ -114,9 +188,75 @@ class GamePage extends React.Component {
     handleCloseModal () {
         this.setState({ showModal: false });
     }
+    drawTile(props) {
 
+        return (
+            <TileWrapper style={{
+                backgroundColor: props.type.color,
+                fontSize: 7,
+                borderRadius: '2pt',
+                margin: '2pt',
+                width: '27pt',
+                height: '27pt',
+                color: 'white',
+                display: 'flex',
+                justifyContent:'center',
+                alignItems: 'center',
+                textAlign: 'center'
+            }}
+            >
+                {!props.piece ?
+                    // if there is no letter on the tile, the tile itself will be rendered
+                    <text>{props.type.text}</text> :
+                    // if there is a letter on the tile, it will be rendered instead of the tile's text
+                    <PieceWrapper style={{
+                        backgroundColor: 'white',
+                        fontSize: 10,
+                        borderRadius: '2pt',
+                        width: '20pt',
+                        height: '20pt',
+                        color: 'black',
+                        display: 'flex',
+                        justifyContent:'center',
+                        alignItems: 'center',
+                        textAlign: 'center'
+                    }}
+                    >
+                        {props.props.type.text}
+                    </PieceWrapper>}
+
+
+            </TileWrapper>
+        );
+    }
+
+    isDropped(piece) {
+
+
+        return this.state.droppedBoxNames.indexOf(piece.text) > -1
+    }
+
+// Update board state when letter is dropped
+    handleDrop(i,j , item) {
+
+        console.log(localStorage.getItem("current"));
+        console.log(this.state.gameId);
+        let newArray = this.state.board;
+        let row = newArray[i];
+        let letterBox = row[j];
+        letterBox.piece = item.piece;
+        this.setState({
+            board: newArray,
+        });
+
+
+    }
 
     render() {
+
+        const { board } = this.state;
+
+
         return (
             <Container>
                     <Row className="justify-content-md-center">
@@ -129,7 +269,36 @@ class GamePage extends React.Component {
                         <Col  className="p-2" md="auto">
                             <BoardWrapper>
                                 <DndProvider backend={Backend}>
-                                <GameBoard/>
+                                    <div>
+                                        <Grid container className="flex-grow-1" spacing={0}>
+
+                                            {board.map((row, i) => (
+                                                <Grid item xs={12}>
+                                                    <Grid key={i} container justify="center" spacing={0}>
+                                                        {row.map((col, j) =>  (
+                                                            <Grid key={col} item>
+                                                                <Square props={col} row={i} column={j} onDrop={(item) => this.handleDrop(i, j, item)}/>
+                                                            </Grid>
+                                                        ))}
+                                                    </Grid>
+                                                </Grid>
+                                            ))}
+                                        </Grid>
+
+
+                                        <DeckWrapper>
+
+                                            <div style={{overflow: 'hidden', clear: 'both', margin:"auto"}}>
+                                                {this.state.boxes.map(({ piece}, index) => (
+                                                    <LetterBox
+                                                        piece={piece}
+                                                        isDropped={this.isDropped(piece, index)}
+                                                        key={index}
+                                                    />
+                                                ))}
+                                            </div>
+                                        </DeckWrapper>
+                                    </div>
                                 </DndProvider>
                             </BoardWrapper>
 
