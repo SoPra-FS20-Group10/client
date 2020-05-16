5bea1e8cd7aa8e182b6f3528d9ff3ba97b694760
Wassermalone <pascal.marty98@gmail.com>
2020-05-16 13:56:00 +0200

    FS20G10-179: Implemented Basic Chart
    FS20G10-179
    
    Implemented a basic chart on the profle page displaying the score of the past 10 games.

--- src/components/profile/Chart.js
+++ src/components/profile/Chart.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import  {Line} from "react-chartjs-2";
+import React, {Component} from 'react';
+import {Line} from "react-chartjs-2";
 import styled from "styled-components";
 
 const GraphWrapper = styled.div`
@@ -12,94 +12,133 @@ const GraphWrapper = styled.div`
     color: white;
      
 `;
-class Chart extends Component {
+//
+// class Chart extends Component {
+//
+//     constructor(props) {
+//         super(props);
+//
+//         this.state = {
+//             matches: this.props.matches,
+//             data: {
+//                 labels: ['10', '9', '8', '7', '6', '5', '4', '3', '2', '1'],
+//                 datasets: [
+//                     {
+//                         // label: 'My First dataset',
+//                         // fill: false,
+//                         lineTension: 0.1,
+//                         backgroundColor: 'rgba(75,192,192,0.4)',
+//                         borderColor: 'rgba(75,192,192,1)',
+//                         borderCapStyle: 'butt',
+//                         borderDash: [],
+//                         borderDashOffset: 0.0,
+//                         borderJoinStyle: 'miter',
+//                         pointBorderColor: 'rgba(75,192,192,1)',
+//                         pointBackgroundColor: '#fff',
+//                         pointBorderWidth: 1,
+//                         pointHoverRadius: 5,
+//                         pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+//                         pointHoverBorderColor: 'rgba(220,220,220,1)',
+//                         pointHoverBorderWidth: 2,
+//                         pointRadius: 1,
+//                         pointHitRadius: 10,
+//                         labelColor: 'white',
+//
+//                         // data: [65, 59, 80, 81, 56, 55, 40]
+//                         data: this.props.matches
+//                     }
+//                 ]
+//             }
+//         }
+//
+//         this.setState({matches: this.props})
+//         console.log(this)
+//     }
 
-    constructor(props) {
-        super(props);
+// render() {
+const Chart = ({matches}) => {
+    let data = {
+        labels: ['10', '9', '8', '7', '6', '5', '4', '3', '2', '1'],
+        datasets: [
+            {
+                // label: 'My First dataset',
+                // fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                labelColor: 'white',
 
-        this.state = {
-            data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                datasets: [
-                    {
-                        label: 'My First dataset',
-                        // fill: false,
-                        lineTension: 0.1,
-                        backgroundColor: 'rgba(75,192,192,0.4)',
-                        borderColor: 'rgba(75,192,192,1)',
-                        borderCapStyle: 'butt',
-                        borderDash: [],
-                        borderDashOffset: 0.0,
-                        borderJoinStyle: 'miter',
-                        pointBorderColor: 'rgba(75,192,192,1)',
-                        pointBackgroundColor: '#fff',
-                        pointBorderWidth: 1,
-                        pointHoverRadius: 5,
-                        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                        pointHoverBorderColor: 'rgba(220,220,220,1)',
-                        pointHoverBorderWidth: 2,
-                        pointRadius: 1,
-                        pointHitRadius: 10,
-                        labelColor: 'white',
-
-                        data: [65, 59, 80, 81, 56, 55, 40]
-                    }
-                ]
+                // data: [65, 59, 80, 81, 56, 55, 40]
+                data: matches
             }
-        }
+        ]
     }
-
-    render() {
-        return(
-                <Line
-                    options={{
-                        legend: {
-                            labels: {
-                                fontColor: "white",
-                                fontSize: 18
-                            }
+    return (
+        <Line
+            options={{
+                legend: {
+                    display: false,
+                    labels: {
+                        fontColor: "white",
+                        fontSize: 18
+                    }
+                },
+                responsive: true,
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Past 10 Games',
+                            fontColor: 'white',
+                            fontSize: 10
                         },
-                        responsive: true,
-                        scales: {
-                            xAxes: [{
-                                display: true,
-                                scaleLabel: {
-                                    display: true,
-                                    labelString: 'X axe name',
-                                    fontColor:'white',
-                                    fontSize:10
-                                },
-                                ticks: {
-                                    fontColor: "white",
-                                    fontSize: 14
-                                },
-                                gridLines: {
-                                    color: "white"
-                                }
-                            }],
-                            yAxes: [{
-                                display: true,
-                                scaleLabel: {
-                                    display: true,
-                                    labelString: 'Y axe name',
-                                    fontColor: 'white',
-                                    fontSize:10
-                                },
-                                ticks: {
-                                    fontColor: "white",
-                                    fontSize: 14
-                                },
-                                gridLines: {
-                                    color: "white"
-                                }
-                            }]
+                        ticks: {
+                            fontColor: "white",
+                            fontSize: 14
+                        },
+                        gridLines: {
+                            color: 'white',
+                            drawOnChartArea: false
                         }
+                    }],
+                    yAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Score',
+                            fontColor: 'white',
+                            fontSize: 10
+                        },
+                        ticks: {
+                            fontColor: "white",
+                            fontSize: 14
+                        },
+                        gridLines: {
+                            color: 'white',
+                            drawOnChartArea: false
+                        }
+                    }]
+                }
 
-                    }}
-                    data={this.state.data}
-                />
-        )
-    }
+            }}
+            data={data}
+        />
+    )
+    // }
 }
 
 export default Chart;
